a950cd240a947b3f9b7789820c98625b7881732d
--global <--global>
2024-10-01 14:14:50 -0300

    painel funfando

---
+++ view/painel.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Teste Painel</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <style>
+      
+    </style>
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-2 sidebar">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-dark mb-2" href="home.php">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-dark mb-2" href="gerenciaCliente.php">Cadastrar usuários</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-dark mb-2" href="cadastroProduto.php">Cadastrar Produtos</a>
+                    </li>
+                    
+                    
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-danger mb-2" href="login.php">Log Out</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-success mb-2" href="inicio.php">Ir para o site</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-10 content">
+                <div class="container">
+                    <h1 class="text-center">Bem-Vindo ao Painel administrativo da Loja </h1>
+                    <p class="welcome-message">Seja bem-vindo à nossa plataforma de administração interna.</p>
+                </div>
+</div>  
+    
+        </div>
+    </div>
+
+</body>
+
+</html>
